b44b6144e729af55876bdf11df347192f755ecbe
YHfeather <18843740508@163.com>
2018-06-05 21:05:41 +0900

    create benchmark with token table

--- benchmark.py
+++ benchmark.py
@@ -1,157 +1,38 @@
-import tensorflow as tf
 import numpy as np
-import tflearn
-import random
-
+import pandas as pd
 
 import data_utils
 
-
-train_dir = 'dataset/programs_800/'
-query_dir = 'dataset/programs_200/'
-model_dir = 'saved_model/model_parameter'
+train_dir = 'dataset/programs_800'
+test_dir = 'dataset/programs_200'
 
 
-class Code_Completion_Model:
-    '''
-    Machine Learning model class, including data processing, encoding, model_building,
-    training, query_testing, model_save, model_load
-    '''
+def create_token_table(dataset):
+    token_list = []
+    for token in dataset:
+        token_list.append(data_utils.token_to_string(token))
+    token_set = data_utils.get_token_set(dataset)
+    num_token = len(token_set)
+    token_cate_list = list(token_set)
 
-    def __init__(self, token_lists):
-        '''
-        Initialize ML model with training data
-        token_lists: [[{type:.., value:..},{..},{..}], [..], [..]]
-        '''
-        self.dataset = token_lists
-        self.tokens_set = data_utils.get_token_set(self.dataset)
-        self.tokens_size = len(self.tokens_set)  # 213
-        # 构建映射字典
-        self.index_to_string = {i: s for i, s in enumerate(self.tokens_set)}
-        self.string_to_index = {s: i for i, s in enumerate(self.tokens_set)}
+    token_table = pd.DataFrame(
+        np.zeros(num_token * num_token).reshape(num_token, num_token), index=token_cate_list, columns=token_cate_list)
 
+    for index, token in enumerate(token_list):
+        if index > 0:
+            cur_token = token
+            pre_token = token_list[index - 1]
+            token_table[cur_token][pre_token] += 1
 
-    def one_hot_encoding(self, string):
-        vector = [0] * self.tokens_size
-        vector[self.string_to_index[string]] = 1
-        return vector
-
-    # generate X_train data and y_label for ML model
-    def data_processing(self):
-        '''
-        first, transform a token in dict form to a type-value string
-        x_data is a token, y_label is the previous token of x_data
-        '''
-        x_data = []
-        y_data = []
-        for index, token in enumerate(self.dataset):
-            if index > 0:
-                token_string = data_utils.token_to_string(token)
-                prev_token = data_utils.token_to_string(self.dataset[index - 1])
-                x_data.append(self.one_hot_encoding(prev_token))
-                y_data.append(self.one_hot_encoding(token_string))
-        return x_data, y_data
-
-    # neural network functions
-    def create_NN(self):
-        self.nn = tflearn.input_data(shape=[None, self.tokens_size])
-        self.nn = tflearn.fully_connected(self.nn, 128)
-        self.nn = tflearn.fully_connected(self.nn, 128)
-        self.nn = tflearn.fully_connected(self.nn, self.tokens_size, activation='softmax')
-        self.nn = tflearn.regression(self.nn)
-        self.model = tflearn.DNN(self.nn)
-
-    # load trained model into object
-    def load_model(self, model_file):
-        self.create_NN()
-        self.model.load(model_file)
-
-    # training ML model
-    def train(self):
-        print('model training...')
-        x_data, y_data = self.data_processing()
-        self.create_NN()
-        self.model.fit(x_data, y_data, n_epoch=1, batch_size=500, show_metric=True)
-
-    # save trained model to model path
-    def save_model(self, model_file):
-        self.model.save(model_file)
-
-    # query test
-    def query_test(self, prefix, suffix):
-        '''
-        Input: all tokens before the hole token(prefix) and all tokens after the hole token,
-        ML model will predict the most probable token in the hole
-        In this function, use only one token before hole token to predict
-        return: the most probable token
-        '''
-        prev_token_string = data_utils.token_to_string(prefix[-1])
-        x = self.one_hot_encoding(prev_token_string)
-        y = self.model.predict([x])
-        predicted_seq = y[0]
-        if type(predicted_seq) is np.ndarray:
-            predicted_seq = predicted_seq.tolist()
-        best_number = predicted_seq.index(max(predicted_seq))
-        best_string = self.index_to_string[best_number]
-        best_token = data_utils.string_to_token(best_string)
-        return [best_token]
+    return token_table
 
 
-def create_hole(tokens, max_hole_size = 2):
-    '''
-    input: a tokens sequence of source code and max_hole_size
-    return: hole token to be predicted (expection)
-            token sequence before the hole(prefix)
-            token sequence after the hole(suffix)
-    '''
-    hole_size = min(random.randint(1, max_hole_size), len(tokens) - 1)
-    hole_start_index = random.randint(1, len(tokens) - hole_size)
-    hole_end_index = hole_start_index + hole_size
-    prefix = tokens[0 : hole_start_index]
-    expection = tokens[hole_start_index : hole_end_index]
-    suffix = tokens[hole_end_index : 0]
-    return prefix, expection, suffix
 
-def token_equals(token1, token2):
-    '''
-    Determining whether input two tokens are equal or not
-    '''
-    if len(token1) != len(token2):
-        return False
-    for index, t1 in enumerate(token1):
-        t2 = token2[index]
-        if t1['type'] != t2['type'] or t1['value'] != t2['value']:
-            return False
-    return True
+def test_benchmark(token_table):
+    test_token_sequence = data_utils.load_data_with_file(test_dir)
 
 
 if __name__ == '__main__':
+    dataset = data_utils.load_tokens(True)
+    token_table = create_token_table(dataset)
 
-    #data load and model create
-    train_data = data_utils.load_tokens(train_flag=False, is_simplify=True)
-    cc_model = Code_Completion_Model(train_data)
-
-    #training model
-    use_stored_model = False
-    if use_stored_model:
-        cc_model.load_model(model_dir)
-    else:
-        cc_model.train()
-        cc_model.save_model(model_dir)
-
-
-    #test model
-    query_test_data = data_utils.load_tokens(query_dir)
-    correct = 0
-    correct_token_list = []
-    incorrect_token_list = []
-    for tokens in query_test_data:
-        prefix, expection, suffix = create_hole(tokens)
-        prediction = cc_model.query_test(prefix, suffix)
-        if token_equals(prediction, expection):
-            correct += 1
-            correct_token_list.append({'expection': expection, 'prediction': prediction})
-        else:
-            incorrect_token_list.append({'expection': expection, 'prediction': prediction})
-    accuracy = correct / len(query_test_data)
-    print('query test accuracy: ', accuracy)
--- data_utils.py
+++ data_utils.py
@@ -2,6 +2,8 @@ import os
 import json
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
+import pickle
+import random
 
 
 train_dir = 'dataset/programs_800/'
@@ -39,7 +41,7 @@ def load_tokens(train_flag=False, is_simplify=True):
 
 def simplify_token(token):
     '''
-    Because there are too many values for type: "Identifier", "String", "Numeric",
+    Because there are too many values for type: "Identifier", "String", "Numeric", "RegularExpression"
     So this function transforms these types of variables to a common value
     '''
     if token['type'] == 'Identifier':
@@ -48,6 +50,8 @@ def simplify_token(token):
         token['value'] = '1'
     elif token['type'] == 'String':
         token['value'] = 'string'
+    elif token['type'] == 'RegularExpression':
+        token['value'] = 'RExpression'
     else:
         pass
 
@@ -68,7 +72,7 @@ def get_token_set(token_list):
     return token_set
 
 
-import pickle
+
 def data_processing(token_list):
     string_token_list = []
     for token in token_list:
@@ -76,6 +80,63 @@ def data_processing(token_list):
     print(len(string_token_list))#1713662
     pickle.dump(string_token_list, open('string_token_list.p', 'wb'))
     return string_token_list
+
+
+
+
+def load_data_with_file(data_dir=query_dir, is_simplify=True):
+    '''
+    读取指定数据集的文件，返回一个list，并且每个list是一个token sequence
+    :param data_dir:
+    :return:
+    '''
+    token_files = []  # stored the file's path which ends with 'tokens.json'
+    for f in os.listdir(data_dir):
+        file_path = os.path.join(data_dir, f)
+        if os.path.isfile(file_path) and f.endswith('_tokens.json'):
+            token_files.append(file_path)
+
+    # load to a list, element is a token sequence of source code
+    token_lists = [json.load(open(f, encoding='utf-8')) for f in token_files]
+
+    if is_simplify:
+        for token_sequence in token_lists:
+            for token in token_sequence:
+                simplify_token(token)
+    else:
+        pass
+
+    return token_lists
+
+
+
+# function for test
+def create_hole(tokens, max_hole_size = 2):
+    '''
+    input: a tokens sequence of source code and max_hole_size
+    return: hole token to be predicted (expection)
+            token sequence before the hole(prefix)
+            token sequence after the hole(suffix)
+    '''
+    hole_size = min(random.randint(1, max_hole_size), len(tokens) - 1)
+    hole_start_index = random.randint(1, len(tokens) - hole_size)
+    hole_end_index = hole_start_index + hole_size
+    prefix = tokens[0 : hole_start_index]
+    expection = tokens[hole_start_index : hole_end_index]
+    suffix = tokens[hole_end_index : 0]
+    return prefix, expection, suffix
+
+def token_equals(token1, token2):
+    '''
+    Determining whether input two tokens are equal or not
+    '''
+    if len(token1) != len(token2):
+        return False
+    for index, t1 in enumerate(token1):
+        t2 = token2[index]
+        if t1['type'] != t2['type'] or t1['value'] != t2['value']:
+            return False
+    return True
 
 
 if __name__ == '__main__':
