3bf394c89fd3cf67ac45fcabf4aeea3560897514
YHfeather <18843740508@163.com>
2018-07-22 19:35:21 +0900

    modify multicontextnn

--- MutiContextNN.py
+++ MutiContextNN.py
@@ -42,12 +42,40 @@ batch_size = 64
 learning_rate = 0.002
 previous_token_num = 2
 
+'''
+使用TensorFlow自带的layers构建基本的神经网络对token进行预测，
+可以声明使用多少个context tokens 进行预测
+
+多个previous token输入神经网络的方法有两种想法：
+1. 将每个token的representation vector相连，合成一个大的vector输入到神经网络，
+    所以说神经网络的输入层大小应为：每个token vector length * number of previous token
+2. 应为目前表示每个token 使用的方法为one hot encoding，也就是说对每个token都是有且仅有一位为1，其余位为0，
+    所以可以考虑直接将所有的previous token相加，这样做的好处是NN输入层大小永远等于vector length。缺点是没有理论依据，不知道效果是否会更好
+
+
+1. concatenate the representations of previous tokens to a huge vector representation
+2. add the representations of previous tokens together
+
+
+'''
+
+x_train_data_path = 'processed_data/x_train_data.p'
+y_train_data_path = 'processed_data/y_train_data.p'
+train_data_parameter = 'processed_data/x_y_parameter.p'
+query_dir = 'dataset/programs_200/'
+
+epoch_num = 1
+batch_size = 64
+learning_rate = 0.002
+previous_token_num = 2
+
 
 class Code_Completion_Model:
 
     def __init__(self, x_data, y_data, token_set, string2int, int2string):
-        self.x_data = x_data
-        self.y_data = y_data
+        batch_num = len(x_data) // batch_size
+        self.x_data = x_data[:batch_num * batch_size]
+        self.y_data = y_data[:batch_num * batch_size]
         self.index_to_string = int2string
         self.string_to_index = string2int
         self.tokens_set = token_set
@@ -67,25 +95,25 @@ class Code_Completion_Model:
         self.equal = tf.equal(tf.argmax(self.output_y, 1), tf.argmax(self.output, 1))
         self.accuarcy = tf.reduce_mean(tf.cast(self.equal, tf.float32), name='accuracy')
 
-    def get_batch(self, x_data, y_data, previous_token_num=1):
-        #Todo: 检查该函数的准确性
-        x_data = np.array(x_data)
-        for i in range(0, len(x_data), batch_size):
-            batch_x_list = [x_data[i-j:i+batch_size-j,:] for j in range(previous_token_num)]
+    def get_batch(self, context_size=previous_token_num):
 
-            # temp_x = np.array(x_data[i])
-            # for j in range(i-previous_token_num, i):
-            #     temp_x += np.array(x_data[j])
-            batch_y = y_data[i:i + batch_size]
-            yield batch_x_list, batch_y
+        x_data = np.array(self.x_data)
+        for i in range(0, len(self.x_data), batch_size):
+            batch_x = np.zeros((batch_size, self.tokens_size))
+            for j in range(context_size):
+                if i >= j:
+                    temp = x_data[i - j:i - j + batch_size].reshape(-1, self.tokens_size)
+                    if temp.shape == (0, 86): break;
+                    batch_x += temp
+            batch_y = self.y_data[i:i + batch_size]
+            yield batch_x, batch_y
 
     def train(self):
         self.create_NN()
         self.sess = tf.Session()
-
         time_begin = time.time()
         self.sess.run(tf.global_variables_initializer())
-        batch_generator = self.get_batch(self.x_data, self.y_data)
+        batch_generator = self.get_batch()
         for epoch in range(epoch_num):
             for i in range(0, len(self.x_data), batch_size):
                 batch_x, batch_y = next(batch_generator)
@@ -95,18 +123,13 @@ class Code_Completion_Model:
                     show_loss, show_acc = self.sess.run([self.loss, self.accuarcy], feed_dict=feed)
                     print('epoch: %d, training_step: %d, loss: %.2f, accuracy:%.3f' % (epoch, i, show_loss, show_acc))
         time_end = time.time()
-
-        print('training time cost: ', time_end - time_begin)
-
-
+        print('training time cost: %.3f s' % (time_end - time_begin))
 
     # query test
     def query_test(self, prefix, suffix):
         '''
         Input: all tokens before the hole token(prefix) and all tokens after the hole token,
-        ML model will predict the most probable token in the hole
-        In this function, use only one token before hole token to predict
-        return: the most probable token
+        ML model will predict the most probable token in the hole. In this function, use only one token before hole token to predict
         '''
         previous_token_list = prefix[-previous_token_num:]
         context_representation = np.zeros(self.tokens_size)
@@ -122,7 +145,7 @@ class Code_Completion_Model:
         best_token = data_utils.string_to_token(best_string)
         return [best_token]
 
-    #test model
+    # test model
     def test_model(self, query_test_data):
         correct = 0.0
         correct_token_list = []
@@ -130,21 +153,13 @@ class Code_Completion_Model:
         for token_sequence in query_test_data:
             prefix, expection, suffix = data_utils.create_hole(token_sequence)
             prediction = self.query_test(prefix, suffix)[0]
-            #             print(type(prediction))
-            #             print(type(expection[0]))
             if data_utils.token_equals([prediction], expection):
                 correct += 1
                 correct_token_list.append({'expection': expection, 'prediction': prediction})
             else:
                 incorrect_token_list.append({'expection': expection, 'prediction': prediction})
-        #         print(correct_token_list)
-        #         print(incorrect_token_list)
         accuracy = correct / len(query_test_data)
         return accuracy
-
-
-
-
 
 
 if __name__ == '__main__':
@@ -159,7 +174,6 @@ if __name__ == '__main__':
 
     #model train
     model = Code_Completion_Model(x_data, y_data, token_set, string2int, int2string)
-    model.get_batch()
     model.train()
 
     # test model
--- test_model.py
+++ test_model.py
@@ -1,170 +1,38 @@
-import json
-import random
-import tensorflow as tf
-import numpy as np
-import tflearn
-import os
+from maze_env import Maze
+from RL_brain import QLearningTable
 
-train_dir = 'dataset/programs_800/'
-query_dir = 'dataset/programs_200/'
-model_file = 'trained_model_parameter'
+def update():
+    #学习100遍
+    for episode in range(100):
+        #初始化state的观测值
+        observation = env.reset()
 
+        # RL 大脑根据state的观测值挑选action
+        action = RL.choose_action(str(observation))
 
-def load_tokens(token_dir, is_simplify=True):
-    '''
-    load token sequence data from input path: token_dir.
-    is_simplify: whether or not simplify the value of some variable type(see function for detail)
-    return a list whose elements are lists of a token sequence
-    '''
-    token_files = []  # stored the file's path which ends with 'tokens.json'
-    for f in os.listdir(token_dir):
-        file_path = os.path.join(token_dir, f)
-        if os.path.isfile(file_path) and f.endswith('_tokens.json'):
-            token_files.append(file_path)
+        # 探索者在环境中实施这个 action, 并得到环境返回的下一个 state 观测值, reward 和 done (是否是掉下地狱或者升上天堂)
+        observation_, reward, done = env.step(action)
 
-    # load to a list, element is a token sequence of source code
-    token_lists = [json.load(open(f, encoding='utf-8')) for f in token_files]
+        # RL 从这个序列 (state, action, reward, state_) 中学习
+        RL.learn(str(observation), action, reward, str(observation_))
 
-    def simplify_token(token):
-        '''
-        Because there are too many values for type: "Identifier", "String", "Numeric",
-        NN may be diffcult to train because of these different value.
-        So this function can transform these types of variables to a common value
-        '''
-        if token['type'] == 'Identifier':
-            token['value'] = 'id'
-        elif token['type'] == 'Numeric':
-            token['value'] = '1'
-        elif token['type'] == 'String':
-            token['value'] = 'string'
-        else:
-            pass
+        # 将下一个 state 的值传到下一次循环
+        observation = observation_
 
-    if is_simplify:
-        for token_sequence in token_lists:
-            for token in token_sequence:
-                simplify_token(token)
-    else:
-        pass
+        # 如果掉下地狱或者升上天堂, 这回合就结束了
+        if done:
+            break
 
-    return token_lists
+    # 结束游戏并关闭窗口
+    print('game over')
+    env.destroy()
 
 
-import time
+if __name__ == "__main__":
+    # 定义环境 env 和 RL 方式
+    env = Maze()
+    RL = QLearningTable(actions=list(range(env.n_actions)))
 
-
-class Code_Completion_Model:
-    '''
-    Machine Learning model class, including data processing, encoding, model_building,
-    training, query_testing, model_save, model_load
-    '''
-
-    def __init__(self, token_lists):
-        '''
-        Initialize ML model with training data
-        token_lists: [[{type:.., value:..},{..},{..}], [..], [..]]
-        '''
-        self.token_lists = token_lists
-        self.tokens_set = set()
-        for token_sequence in token_lists:
-            for token in token_sequence:
-                self.tokens_set.add(self.token_to_string(token))
-        self.tokens_list = list(self.tokens_set)
-        self.tokens_list.sort()
-        self.tokens_size = len(self.tokens_set)  # 213
-        self.index_to_string = {i: s for i, s in enumerate(self.tokens_list)}
-        self.string_to_index = {s: i for i, s in enumerate(self.tokens_list)}
-
-    # data processing functions
-    def token_to_string(self, token):
-        return token['type'] + '~$$~' + token['value']
-
-    def string_to_token(self, string):
-        tokens = string.split('~$$~')
-        return {'type': tokens[0], 'value': tokens[1]}
-
-    # encoding token sequence as one_hot_encoding
-    def one_hot_encoding(self, string):
-        vector = [0] * self.tokens_size
-        vector[self.string_to_index[string]] = 1
-        return vector
-
-    # generate X_train data and y_label for ML model
-    def data_processing(self):
-        '''
-        first, transform a token in dict form to a type-value string
-        x_data is a token, y_label is the previous token of x_data
-        '''
-        x_data = []
-        y_data = []
-        for token_sequence in self.token_lists:  # token_sequence of each source code
-            for index, token in enumerate(token_sequence):  # each token(type_value) in source code
-                if index > 0:
-                    token_string = self.token_to_string(token)
-                    prev_token = self.token_to_string(token_sequence[index - 1])
-                    x_data.append(self.one_hot_encoding(prev_token))
-                    y_data.append(self.one_hot_encoding(token_string))
-        print('data processing is finished..')
-        return x_data, y_data
-
-    # neural network functions
-    def create_NN(self):
-        tf.reset_default_graph()
-        self.nn = tflearn.input_data(shape=[None, self.tokens_size])
-        self.nn = tflearn.fully_connected(self.nn, 128)
-        self.nn = tflearn.fully_connected(self.nn, 128)
-        self.nn = tflearn.fully_connected(self.nn, self.tokens_size, activation='softmax')
-        self.nn = tflearn.regression(self.nn)
-        self.model = tflearn.DNN(self.nn)
-
-    # load trained model into object
-    def load_model(self, model_file):
-        self.create_NN()
-        self.model.load(model_file)
-
-    # training ML model
-    def train(self):
-        start_time = time.time()
-        x_data, y_data = self.data_processing()
-        self.create_NN()
-        self.model.fit(x_data, y_data, n_epoch=1, batch_size=500, show_metric=True)
-        end_time = time.time()
-        return end_time - start_time
-
-    # save trained model to model path
-    def save_model(self, model_file):
-        self.model.save(model_file)
-
-    # query test
-    def query_test(self, prefix, suffix):
-        '''
-        Input: all tokens before the hole token(prefix) and all tokens after the hole token,
-        ML model will predict the most probable token in the hole
-        In this function, use only one token before hole token to predict
-        return: the most probable token
-        '''
-        prev_token_string = self.token_to_string(prefix[-1])
-        x = self.one_hot_encoding(prev_token_string)
-        y = self.model.predict([x])
-        predicted_seq = y[0]
-        if type(predicted_seq) is np.ndarray:
-            predicted_seq = predicted_seq.tolist()
-        best_number = predicted_seq.index(max(predicted_seq))
-        best_string = self.index_to_string[best_number]
-        best_token = self.string_to_token(best_string)
-        return [best_token]
-
-
-
-
-if __name__ == '__main__':
-    start_time = time.time()
-    dataset = load_tokens(train_dir)
-    load_time = time.time()
-    print('time cost of loading data: %.2f s'%(load_time - start_time))
-
-    code_completion = Code_Completion_Model(dataset)
-    x, y = code_completion.data_processing()
-    end_time = time.time()
-
-    print('time cost of processing data: %.2f s, size of training data: %d'%(end_time-load_time, len(x)))
+    # 开始可视化环境 env
+    env.after(100, update)
+    env.mainloop()
