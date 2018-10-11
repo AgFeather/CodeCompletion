5b1dbc00a12e6e1a467d61bd4ebde655f63935d4
YHfeather <18843740508@163.com>
2018-10-11 13:39:30 +0900

    modify LSTM model

--- BasicRNN.py
+++ BasicRNN.py
@@ -3,7 +3,8 @@ import numpy as np
 import pickle
 import data_utils
 import time
-
+import json
+import os
 
 '''
 Using MultiRNN to pridect token. with LSTM cell
@@ -11,12 +12,13 @@ Using MultiRNN to pridect token. with LSTM cell
 
 
 
-processed_data_path = 'processed_data/vec_train_data.p'
-data_parameter_path = 'processed_data/train_parameter.p'
+processed_data_path = 'processed_data/rnn_train_data.p'
+data_parameter_path = 'processed_data/rnn_train_parameter.p'
 tensorboard_log_path = 'logs/MultiRNN'
 
+train_dir = 'dataset/programs_800'
 test_dir = 'dataset/programs_200'
-
+checkpoint_dir = 'rnn/checkpoints'
 
 num_epoches = 1
 show_every_n = 50
@@ -24,6 +26,38 @@ save_every_n = 200
 
 
 
+def load_tokens(train_flag=True, is_simplify=True):
+    if train_flag:
+        token_dir = train_dir
+    else:
+        token_dir = test_dir
+    token_list = []
+    for f in os.listdir(token_dir):
+        file_path = os.path.join(token_dir, f)
+        if os.path.isfile(file_path) and f.endswith('_tokens.json'):
+            #print(file_path)
+            token_seq = json.load(open(file_path, encoding='utf-8'))
+            token_list.extend(token_seq)
+    string_token_list = []
+    for token in token_list:
+        if is_simplify:
+            data_utils.simplify_token(token)
+        string_token = data_utils.token_to_string(token)
+        string_token_list.append(string_token)
+    token_set = list(set(string_token_list))
+    #print(string_token_list[:10])
+    string2int = {c:i for i,c in enumerate(token_set)}
+    int2string = {i:c for i,c in enumerate(token_set)}
+    int_token_list = [string2int[c] for c in string_token_list]
+    #print(int_token_list[:10])
+    pickle.dump((int_token_list), open(processed_data_path, 'wb'))
+    pickle.dump((string2int, int2string, token_set), open(data_parameter_path, 'wb'))
+
+
+
+'''
+Using MultiRNN to pridect token. with LSTM cell
+'''
 
 
 class LSTM_Model(object):
@@ -36,12 +70,13 @@ class LSTM_Model(object):
                  grad_clip=5,
                  keep_prob=0.5,
                  is_training=True):
-        self.string2int, self.int2string, self.token_set = string2int, int2string, token_set
+        #self.string2int, self.int2string, \
+        self.token_set =  token_set
         if is_training:
             self.time_steps = time_steps
             self.batch_size = batch_size
         else:
-            self.time_steps = 1 # todo: modify
+            self.time_steps = time_steps # todo: modify
             self.batch_size = 1
         self.num_classes = len(self.token_set)
         self.num_layers = num_layers
@@ -55,27 +90,23 @@ class LSTM_Model(object):
 
     def get_batch(self, data_seq, n_seq, n_steps):
         '''
-
         :param n_seq: 一个batch中序列的个数
         :param n_steps: 单个序列中包含字符的个数
-        :return:
         '''
+        data_seq = np.array(data_seq)
         batch_size = n_steps * n_seq
         n_batches = len(data_seq) // batch_size
         data_seq = data_seq[:batch_size * n_batches] #仅保留完整的batch，舍去末尾
-
         data_seq = data_seq.reshape((n_seq, -1))
-
         for n in range(0, data_seq.shape[1], n_steps):
             x = data_seq[:, n:n+n_steps]
             y = np.zeros_like(x)
-            y[:, -1], y[:, -1] = x[:, 1:], x[:,0]
+            y[:, :-1], y[:, -1] = x[:, 1:], x[:,0]
             yield x, y
 
-
     def build_input(self):
-        input_x = tf.placeholder(tf.float32, [self.batch_size, self.time_steps], name='input_x')
-        target_y = tf.placeholder(tf.float32, [self.batch_size, self.time_steps], name='target_y')
+        input_x = tf.placeholder(tf.int32, [self.batch_size, self.time_steps], name='input_x')
+        target_y = tf.placeholder(tf.int32, [self.batch_size, self.time_steps], name='target_y')
         keep_prob = tf.placeholder(tf.float32, name='keep_prob')
 
         return input_x, target_y, keep_prob
@@ -92,7 +123,6 @@ class LSTM_Model(object):
         return cells, init_state
 
     def bulid_output(self, lstm_output):
-
         # 将lstm_output的形状由[batch_size, time_steps, n_units] 转换为 [batch_size*time_steps, n_units]
         seq_output = tf.concat(lstm_output, axis=1)
         seq_output = tf.reshape(seq_output, [-1, self.n_units])
@@ -103,47 +133,54 @@ class LSTM_Model(object):
 
         logits = tf.matmul(seq_output, softmax_w) + softmax_b
         softmax_output = tf.nn.softmax(logits=logits, name='softmax_output')
-
         return softmax_output, logits
 
     def bulid_loss(self, logits, targets):
-        loss = tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, targets=targets)
+        one_hot_y = tf.one_hot(targets, self.num_classes)
+        one_hot_y = tf.reshape(one_hot_y, logits.get_shape())
+        loss = tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=one_hot_y)
         loss = tf.reduce_mean(loss)
-
         return loss
 
     def bulid_optimizer(self,loss):
-        tvars = tf.trainable_variables()
-        grads, _ = tf.clip_by_global_norm(tf.gradients(loss, tvars), self.grad_clip)
+        # tvars = tf.trainable_variables()
+        # grads, _ = tf.clip_by_global_norm(tf.gradients(loss, tvars), self.grad_clip)
+        # optimizer = tf.train.AdamOptimizer(self.learning_rate)
+        # optimizer = optimizer.apply_gradients(zip(grads, tvars))
         optimizer = tf.train.AdamOptimizer(self.learning_rate)
-        optimizer = optimizer.apply_gradients(zip(grads, tvars))
-
+        gradient_pairs = optimizer.compute_gradients(loss)
+        clip_gradient_pairs = []
+        for grad, var in gradient_pairs:
+            grad = tf.clip_by_value(grad, -2, 2)
+            clip_gradient_pairs.append((grad, var))
+        optimizer = optimizer.apply_gradients(clip_gradient_pairs)
         return optimizer
 
+    def build_accuracy(self, targets, logits):
+        pass
 
     def bulid_model(self):
         tf.reset_default_graph()
         self.input_x, self.target_y, self.keep_prob = self.build_input()
         self.cell, self.init_state = self.bulid_lstm(self.keep_prob)
-
         one_hot_x = tf.one_hot(self.input_x, self.num_classes)
-
-        lstm_outputs, self.final_state = tf.nn.dynamic_rnn(self.cell, one_hot_x, initial_state=self.init_state)
-
+        print(one_hot_x.get_shape()) # (64, 100, 86)
+        lstm_outputs, self.final_state = tf.nn.dynamic_rnn(
+            self.cell, one_hot_x, initial_state=self.init_state)
+        print(1, lstm_outputs.get_shape()) # (64, 100, 128)
         self.softmax_output, logits = self.bulid_output(lstm_outputs)
-
+        print(self.softmax_output.get_shape()) # (6400, 86)
+        print(logits.get_shape()) #(6400, 86)
         self.loss = self.bulid_loss(logits,self.target_y)
-
         self.optimizer = self.bulid_optimizer(self.loss)
 
 
-
-
-
-    def train(self, data):
+    def train(self, data, string2int, int2string):
         print('training begin...')
+        self.string2int = string2int
+        self.int2string = int2string
         saver = tf.train.Saver(max_to_keep=100)
-
+        keep_prob = 0.5
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
             global_step = 0
@@ -157,7 +194,7 @@ class LSTM_Model(object):
                     batch_step += 1
                     feed = {self.input_x:x,
                             self.target_y:y,
-                            self.keep_prob:self.keep_prob,
+                            self.keep_prob:keep_prob,
                             self.init_state:new_state}
                     show_loss, new_state, _ = sess.run(
                         [self.loss, self.final_state, self.optimizer], feed_dict=feed)
@@ -166,11 +203,10 @@ class LSTM_Model(object):
                         print('epoch: {}/{}...'.format(epoch+1, num_epoches),
                               'global_step: {}...'.format(global_step),
                               ' train_loss: {:.4f}...'.format(show_loss),
-                              'time cost in per_batch: {:.4f}...'.format(end_time-start_time))
+                              'time cost in per_batch: {:.2f}...'.format(end_time-start_time))
 
                     if global_step % save_every_n == 0:
                         saver.save(sess, 'checkpoints/epoch{}_batch_step{}'.format(epoch, batch_step))
-
             saver.save(sess, 'checkpoints/last_check')
 
 
@@ -178,23 +214,36 @@ class LSTM_Model(object):
 
 
 
+class Test_Model(object):
+    def __init__(self, vocab_size,string2int, int2string):
+        self.model = LSTM_Model(vocab_size, is_training=False)
+        self.string2int = string2int
+        self.int2string = int2string
+
     # query test
     def query_test(self, prefix, suffix):
         '''
         Input: all tokens before the hole token(prefix) and all tokens after the hole token,
         ML model will predict the most probable token in the hole
         '''
-        test_previous_tokens = []
-        for token in prefix:
-            prev_token_string = data_utils.token_to_string(token)
-            pre_token_x = data_utils.one_hot_encoding(prev_token_string, self.string_to_index)
-            test_previous_tokens.append(pre_token_x)
-
-        feed = {self.input_x: test_previous_tokens, self.keep_prob:1}
-        prediction = self.sess.run(self.prediction_index, feed)[0]
-        best_string = self.index_to_string[prediction]
-        best_token = data_utils.string_to_token(best_string)
-        return [best_token]
+        last_chackpoints = tf.train.latest_checkpoint(checkpoint_dir=checkpoint_dir)
+        pre_tokens = [c for c in prefix]
+        saver = tf.train.Saver()
+        with tf.Session() as sess:
+            saver.restore(sess, last_chackpoints)
+            new_state = sess.run(self.model.init_state)
+            for i,token in enumerate(pre_tokens):
+                x = np.zeros((1, 1))
+                x[0,0] = self.string2int(token)
+                feed = {model.input_x:x,
+                        model.keep_prob:1.,
+                        model.init_state:new_state}
+                new_state = sess.run(model.final_state, feed_dict=feed)
+                if i == len(pre_tokens)-1:
+                    prediction = sess.run(model.softmax_output, feed_dict=feed)
+        prediction = self.int2string(np.argmax(prediction))
+        print(prediction)
+        return prediction
 
     def test_model(self, query_test_data):
         correct = 0.0
@@ -212,21 +261,15 @@ class LSTM_Model(object):
         return accuracy
 
 
+
 if __name__ == '__main__':
     train_data = data_utils.load_data_with_pickle(processed_data_path)
     string2int, int2string, token_set = data_utils.load_data_with_pickle(data_parameter_path)
 
     model = LSTM_Model(token_set)
+    model.train(train_data, string2int, int2string)
 
     # model train
-    model.train(train_data)
-
-    # model test
-    query_test_data = data_utils.load_data_with_file(test_dir)
-    test_accuracy = 0.0
-    test_epoches = 10
-    for i in range(test_epoches):
-        accuracy = model.test_model(query_test_data)
-        print('test epoch: %d, query test accuracy: %.3f' % (i, accuracy))
-        test_accuracy += accuracy
-    print('total test accuracy: %.3f' % (test_accuracy / test_epoches))
+    # model.train(train_data, string2int, int2string)
+    #
+    # test_model = Test_Model(len(token_set), string2int, int2string)
